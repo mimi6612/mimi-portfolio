87de29c6a7c3781b1f178bbe13c86d19e7bad198
ryoichiroSlogan <komatsuzaki.ryoichiro@slogan.jp>
2021-04-01 14:25:18 +0900

    portfolioページの追加

---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>ポートフォリオ</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/pages/Home.vue";
+import About from "@/pages/About.vue";
+import Portfolio from "@/pages/Portfolio.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +12,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    component: Portfolio
   }
 ];
 
