008c828811c722bbacb3fb3ebf8004dcde68ba85
ryoichiroSlogan <komatsuzaki.ryoichiro@slogan.jp>
2021-04-01 14:54:06 +0900

    buildが正しくされるようにした

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
- devServer:{
-        public: "6d12105467274856ac9aaf46f553a32f.vfs.cloud9.us-east-2.amazonaws.com"
-    }
-}
+  publicPath: "./",
+  devServer: {
+    public:
+      "6d12105467274856ac9aaf46f553a32f.vfs.cloud9.us-east-2.amazonaws.com"
+  }
+};
